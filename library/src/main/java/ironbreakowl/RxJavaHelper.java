59814c9596e7f144d09ff653bed000d33bbc6bbc
suckgamony <suckgamony@gmail.com>
2017-10-28 11:07:04 +0900

    Update to Android Studio 3.0

--- library/src/main/java/ironbreakowl/RxJavaHelper.java
+++ library/src/main/java/ironbreakowl/RxJavaHelper.java
@@ -1,23 +1,9 @@
 package ironbreakowl;
 
 import io.reactivex.Flowable;
-import rx.Observable;
 
 class RxJavaHelper {
-    private static Boolean hasRxJava1;
     private static Boolean hasRxJava2;
-
-    static boolean hasRxJava1() {
-        if (hasRxJava1 == null) {
-            try {
-                Class.forName(Observable.class.getName());
-                hasRxJava1 = true;
-            } catch (Throwable e) {
-                hasRxJava1 = false;
-            }
-        }
-        return hasRxJava1;
-    }
 
     static boolean hasRxJava2() {
         if (hasRxJava2 == null) {
